6f626d44abe82b87076a97b5c94fd0a76002f4bd
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2025-02-11 10:56:05 +0600

    hero component added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,20 @@
 const About = () => {
     return (
         <div>
-            about
+            <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+                <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">We love</h1>
+                <div className="stats bg-primary shadow">
+                    <div className="stat">
+                        <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+                            restful
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+            At restful, we provide a seamless and secure room booking experience, ensuring the best deals for travelers, while our 24/7 customer support guarantees hassle-free service, making it easier than ever to find budget-friendly stays or luxury suites that match your needs.🚀
+            </p>
         </div>
     );
 };
